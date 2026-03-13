e31c05e3d0fa6524c39b6c613493a243910e965f
sardarjunaidsahil <sardarjunaidsahil@gmail.com>
2026-03-13 09:33:52 +0500

    fix: logo text for Dancing Script font

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -273,7 +273,7 @@ export default function Navbar() {
           {/* Logo */}
           <Link to="/" className="nb-logo">
             <span className="nb-logo-bracket">&lt;</span>
-            <span className="nb-logo-text">SʌɾԃAɾ JυnAιԃ SAhιɭ</span>
+            <span className="nb-logo-text">Sardar Junaid Sahil</span>
             <span className="nb-logo-space"> </span>
             <span className="nb-logo-bracket">/&gt;</span>
           </Link>
